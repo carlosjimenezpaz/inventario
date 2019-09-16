e53811868e91227506086d6897913b12b826eb5a
Carlos Luis Jimenez Paz <cluis_98@live.com>
2019-09-16 13:56:29 -0600

    Agregando Uso de LinQ.

---
+++ Inventario/Inventario.BL/Contexto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.BL
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\InventarioDB.mdf")
+        {
+
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalle> PedidoDetalle { get; set; }
+
+    }
+}
---
+++ Inventario/Inventario.BL/Producto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.BL
+{
+    public class Producto
+    {
+        public Producto()
+        {
+            Activo = true;
+        }
+
+        public int Id { get; set; }
+        [Display(Name = "Descripcion")]
+
+        [Required(ErrorMessage = "ingrese la descripcion")]
+        [MinLength(3, ErrorMessage = "Ingrese un minimo de 3 caracteres")]
+        [MaxLength(20, ErrorMessage = "Se permite solo 20 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el precio")]
+        [Range(0, 1000, ErrorMessage = "Ingrese un precio valido ")]
+        public double Precio { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+        [Display(Name = "Imagen")]
+        public string UrlImagen { get; set; }
+
+        public bool Activo { get; set; }
+
+    }
+}
